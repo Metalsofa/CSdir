5b1bdb56f2fc49c8f4d701bd3dbffbfd793dc15f
David Medlock <46931911+Metalsofa@users.noreply.github.com>
2019-03-09 00:13:23 -0500

    Update CSdir.sh

--- CSdir.sh
+++ CSdir.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+# User inputs an argument for this
 hwnum=$1
+#This is an unimplemented feature
 vim=false
 
-#--------USER-SPECIFIC OPTIONS--------
+#------------USER-SPECIFIC OPTIONS------------
 # Change this to your Homework path for the concerned class
-dsdir="/mnt/c/ds/"
+hwdir="/mnt/c/ds/"
 # Change this to the name of your course
 coursename="Data Structures"
 # Change this to the string you use to prefix your homeworks.
@@ -18,15 +19,15 @@ zipstring="provided_files.zip"
 # Set this to whatever the current professor is calling the provided pdf file
 # For me, it was "hw.pdf"
 pdfstring="hw.pdf"
-# Set this to TRUE if you want to enable cloning files directly from the 
-# DS site, or FALSE if that scares you (no shame in that)
-dsclone=true
-#-------------------------------------
+# Set this to TRUE if you want to enable cloning provided files directly from the 
+# course site, or FALSE if that scares you (no shame in that)
+autoclone=true
+#---------------------------------------------
 
-if [ -d "$dsdir$hwstring$hwnum" ]; then
+if [ -d "$hwdir$hwstring$hwnum" ]; then
 	echo "Now navigating to $coursename: Homework $hwnum"
 	cd ~
-	cd $dsdir$hwstring$hwnum
+	cd $hwdir$hwstring$hwnum
 	#show the user wherest they are
 	ls
 	#open some stuff in VIM
@@ -45,8 +46,8 @@ else
 		#Create the directory
 		cd /mnt/c/workshop++/ds/
 		mkdir $hwstring$hwnum
-		echo "Created directory $dsdir$hwstring$hwnum"
-		if [[ ! $dsclone ]]; then
+		echo "Created directory $hwdir$hwstring$hwnum"
+		if [[ ! $autoclone ]]; then
 			#Check if provided_files.zip is in the DS parent directory
 			if [ -f "$zipstring" ]; then
 				echo "Found $zipstring in $coursename directory"
